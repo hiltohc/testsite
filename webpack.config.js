1ac477b28c63716dfd6727d38659e45b85530a7c
hiltohc <105225792+hiltohc@users.noreply.github.com>
2022-05-23 16:55:31 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'public', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+  watch: true,
+  mode: 'development',
+};
